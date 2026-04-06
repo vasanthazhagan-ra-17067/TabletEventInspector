d3c274cd12005bb5900ec04a874ecf2b10de082a
Vasanth <vasanthazhagan.ra@zohocorp.com>
2026-04-06 22:18:29 +0530

    Initial Commit

---
+++ TabletEventInspector/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  TabletEventInspector
+//
+//  Created by vignesh-13338-t on 06/04/26.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
---
+++ TabletEventInspectorTests/TabletEventInspectorTests.swift
@@ -0,0 +1,17 @@
+//
+//  TabletEventInspectorTests.swift
+//  TabletEventInspectorTests
+//
+//  Created by vignesh-13338-t on 06/04/26.
+//
+
+import Testing
+@testable import TabletEventInspector
+
+struct TabletEventInspectorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
